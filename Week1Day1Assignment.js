9a4fcc87ae9491b11afb3f97281519f36416bdb8
Arun-2602 <arun1226k@gmail.com>
2026-03-19 11:36:05 +0530

    Add files via upload
    Week1 Day1

---
+++ Week1Day1Assignment.js
@@ -0,0 +1,19 @@
+var userName = "Arunkumar K"
+console.log(userName);
+console.log(typeof userName);
+
+var organization = "Capgemini"
+console.log(organization);
+console.log(typeof organization);
+
+var contactNumber = 7567837461
+console.log(contactNumber);
+console.log(typeof contactNumber);
+
+var knowsAutomation = true
+console.log(knowsAutomation);
+console.log(typeof knowsAutomation);
+
+var usesPlaywright
+console.log(usesPlaywright);
+console.log(typeof usesPlaywright);
