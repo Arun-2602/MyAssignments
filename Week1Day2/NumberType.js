f67c78098407b93a6c2b5108bc025b7b5b6c4452
Arun-2602 <arun1226k@gmail.com>
2026-03-22 11:24:50 +0530

    Day2

---
+++ Week1Day2/NumberType.js
@@ -0,0 +1,13 @@
+function numberType() {
+    let n = 12
+    if(n>0) {
+          console.log("Number is greater than 0:",n)
+    }else if(n<0) {
+          console.log("Number is lesser than 0:",n)
+    }else if(n===0) {
+          console.log("Number is zero:",n)
+    }else {
+          console.log("Given value is not number")
+    }
+}
+numberType()
